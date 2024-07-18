87056acfb91d2c7ace8097ee0fab2f674f56bafd
Rémy Dumas <remy.benjamin.dumas@gmail.com>
2024-07-18 13:31:23 +0200

    Update clients logo

---
+++ src/components/icons/Grist/index.style.jsx
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const GristStyle = styled.svg``
---
+++ src/components/icons/Pulitzer/index.style.jsx
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const PulitzerStyle = styled.svg``
